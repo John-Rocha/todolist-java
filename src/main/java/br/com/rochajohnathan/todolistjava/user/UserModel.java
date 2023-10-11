f7097574f2d9f028c60029f1a59d3aa7eef6e73a
John Rocha <johnathanrocha@gmail.com>
2023-10-11 14:23:49 -0300

    adicionado o lombok

---
+++ src/main/java/br/com/rochajohnathan/todolistjava/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.rochajohnathan.todolistjava.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String userName;
+    private String name;
+    private String password;
+
+}
